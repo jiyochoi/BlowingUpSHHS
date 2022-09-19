c745d20556f3f61307ecf4b514394a2c454cdce5
jiyochoi <jiyochoi2@gmail.com>
2022-09-20 07:42:00 +0900

    Add: 이것저것

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import gunSound from './snd/gun2.mp3';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  const getImg = () => {
+    return "img/hakgyo.png";
+  }
+  
+  const [click, setClick] = useState(0);
+
+  const [hakgyoImg, setHakgyoImg] = useState(getImg);
+
+  //neverending loop
+  /*play sound
+  const useAudio = () => {
+    const [loaded, setLoaded] = useState(0);
+
+    useEffect(() => {
+      //AudioContext 생성
+      const AudioContext = (window.AudioContext || window.webkitAudioContext)();
+      const audioContext = new AudioContext();
+      
+      //pending
+      const fromis9 = new Promise(async (resolve, reject) => {
+
+        const meorahaji = fetch("snd/gun2.aif");
+        const arrayBuffer = new ArrayBuffer(meorahaji);
+        const audioBuffer = await AudioContext.decodeAudioData(arrayBuffer);
+        const AudioBufferSourceNode = await audioContext.createBufferSource();
+        
+        AudioBufferSourceNode.buffer = audioBuffer;
+        AudioBufferSourceNode.connect(audioContext.destination);
+
+        AudioBufferSourceNode.start();
+
+        resolve();
+      });
+      //fulfilled
+
+      Promise.all(fromis9).then(() => {
+        setLoaded(true);
+      })
+    });
+
+    return{
+      loaded,
+      audios,
+    };
+  }
+
+  const {loaded, audios} = useAudio("snd/gun2.aif");
+
+  */
+
+  const useAudio = () => {
+    
+  }
+  
+  return(
+    <div>hello world</div>
   );
 }
 
---
+++ src/Seohyun.js
@@ -0,0 +1,27 @@
+/*import React, { useState } from "react";
+
+export default class Seohyun extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+    changeImage = () => {
+        document.getElementById("hakgyo").src = "img/hakgyoboom.png";
+        setTimeout(() => { document.getElementById("hakgyo").src = "img/hakgyo.png"; }, 200);
+    }
+
+    render() {
+        return (
+            <div>
+                <style>
+                @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
+                </style>
+                <h1>Burning Seohyun High School</h1>
+                <img id="hakgyo" onClick={this.changeImage} src="img/hakgyo.png" />
+
+            </div>
+        );
+    }
+} */
